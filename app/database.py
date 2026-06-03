6add0278522354e67af9fd311a2057ec4e93d325
Sarthak Mukherjee <sarthakmukherjee@virtualemployee.com>
2026-06-03 16:51:54 +0530

    changes

--- app/database.py
+++ app/database.py
@@ -1,11 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import OperationalError
 
 DATABASE_URL = (
+<<<<<<< Updated upstream
     "postgresql://postgres:Pass123@localhost:5432/content_agent"
+=======
+    "postgresql://postgres:Pass#123@localhost:5432/content_agent"
+>>>>>>> Stashed changes
 )
 
 engine = create_engine(DATABASE_URL)
+
+try:
+    with engine.connect() as connection:
+        print("Database connected successfully.")
+except OperationalError as e:
+    print(e)
 
 SessionLocal = sessionmaker(
     autocommit=False,
